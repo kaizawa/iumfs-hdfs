c59e8a61e7dbaf382374054e75a77c8a479c05f1
Kazuyoshi Aizawa <admin2@whiteboard.ne.jp>
2012-04-28 16:41:17 +0900

    Mod: Changed hadoop-core version to 1.0.2

--- start-hdfsd.sh
+++ start-hdfsd.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
-
-#CLASSPATH=cmd/hdfsd.jar:${HADOOP_HOME}/conf:${HADOOP_HOME}/hadoop-common-0.21.0.jar:${HADOOP_HOME}/hadoop-hdfs-0.21.0.jar:${HADOOP_HOME}/lib/commons-logging-1.1.1.jar:. 
-
+#
+# You might need to correct jar file name to
+# refrect your environment.
+#  - Following version of jar files are from
+#    defautl jar file of Hadoop 1.0.2
+# HADOOP_HOME variable seems to get deprecated.
+# But there's no reasonable way to know the 
+# location of jar files, I continue to use
+# HADOOP_HOME for now.
+#
 CLASSPATH="\
 ./cmd/hdfsd.jar:${HADOOP_HOME}/conf:\
 ./cmd/lib/iumfs-daemon-core.jar:\
-${HADOOP_HOME}/hadoop-core-1.0.1.jar:\
+${HADOOP_HOME}/hadoop-core-1.0.2.jar:\
 ${HADOOP_HOME}/lib/commons-lang-2.4.jar:\
 ${HADOOP_HOME}/lib/commons-configuration-1.6.jar:\
 ${HADOOP_HOME}/lib/commons-logging-1.1.1.jar"
 
-hadoop -Djava.util.logging.config.file=cmd/log.prop -cp $CLASSPATH iumfs.hdfs.Main 
-#hadoop -cp $CLASSPATH iumfs.hdfs.Main
+# For Debug
+#hadoop -Djava.util.logging.config.file=cmd/log.prop -cp $CLASSPATH iumfs.hdfs.Main 
+
+hadoop -cp $CLASSPATH iumfs.hdfs.Main
