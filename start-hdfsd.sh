e390eea5af797339b236b43389a0da485b89a7e7
Kazuyoshi Aizawa <admin2@whiteboard.ne.jp>
2012-04-30 11:06:18 +0900

    Mod: removed version number of iumfs-daemon-core library

--- start-hdfsd.sh
+++ start-hdfsd.sh
@@ -3,9 +3,9 @@
 HADOOPHOME=`which hadoop | xargs dirname | xargs dirname`
 CONFDIR="$HADOOPHOME/conf"
 
-CLASSPATH="$CONFDIR:./cmd/hdfsd.jar:./cmd/lib/iumfs-daemon-core-0.2.0.jar:`hadoop classpath`"
+CLASSPATH="$CONFDIR:./cmd/hdfsd.jar:`/usr/bin/ls ./cmd/lib/iumfs-daemon-core*.jar`:`hadoop classpath`"
 
 # For Debug
-hadoop -Djava.util.logging.config.file=cmd/log.prop -cp $CLASSPATH iumfs.hdfs.Main 
+#hadoop -Djava.util.logging.config.file=cmd/log.prop -cp $CLASSPATH iumfs.hdfs.Main 
 
-#hadoop -cp $CLASSPATH iumfs.hdfs.Main
+hadoop -cp $CLASSPATH iumfs.hdfs.Main
